cc6a5bddb8a28a13e46dc65398d698f29e2f9e4e
Faiaz Hossain <faiazhossain10@gmail.com>
2022-11-25 19:47:09 +0600

    car data added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -7,6 +9,28 @@ const app = express();
 //middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fpsp1lt.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const carCollection = client.db("carHut").collection("carData");
+
+    app.get("/brands", async (req, res) => {
+      const query = {};
+      const cursor = carCollection.find(query);
+      const brands = await cursor.toArray();
+      res.send(brands);
+    });
+  } finally {
+  }
+}
+run().catch((err) => console.error(err));
 
 app.get("/", async (req, res) => {
   res.send("Car hut server is running");
